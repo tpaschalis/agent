7de04ad0e352bfe95bb5fb551dbb3edf6e646ffb
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2023-01-09 21:02:38 +0200

    PR base
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/loki/process/internal/stages/pipeline.go
+++ component/loki/process/internal/stages/pipeline.go
@@ -20,8 +20,13 @@ import (
 // We define these as pointers types so we can use reflection to check that
 // exactly one is set.
 type StageConfig struct {
-	JSONConfig   *JSONConfig   `river:"json,block,optional"`
-	LabelsConfig *LabelsConfig `river:"labels,block,optional"`
+	JSONConfig      *JSONConfig      `river:"json,block,optional"`
+	LabelsConfig    *LabelsConfig    `river:"labels,block,optional"`
+	DockerConfig    *DockerConfig    `river:"docker,block,optional"`
+	CRIConfig       *CRIConfig       `river:"cri,block,optional"`
+	RegexConfig     *RegexConfig     `river:"regex,block,optional"`
+	TimestampConfig *TimestampConfig `river:"timestamp,block,optional"`
+	OutputConfig    *OutputConfig    `river:"output,block,optional"`
 }
 
 // UnmarshalRiver implements river.Unmarshaler.
