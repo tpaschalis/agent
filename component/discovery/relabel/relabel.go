8b186ea1ba88f74c0837372bf788a778ac11e323
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2023-02-05 01:25:21 +0200

    Premature PR base
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/discovery/relabel/relabel.go
+++ component/discovery/relabel/relabel.go
@@ -2,11 +2,15 @@ package relabel
 
 import (
 	"context"
+	"reflect"
 	"sync"
 
+	"github.com/go-kit/log/level"
 	"github.com/grafana/agent/component"
 	flow_relabel "github.com/grafana/agent/component/common/relabel"
 	"github.com/grafana/agent/component/discovery"
+	"github.com/grafana/agent/pkg/river"
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/relabel"
 )
@@ -30,6 +34,25 @@ type Arguments struct {
 
 	// The relabelling rules to apply to the each target's label set.
 	RelabelConfigs []*flow_relabel.Config `river:"rule,block,optional"`
+
+	// The maximum number of items to hold in the component's LRU cache.
+	MaxCacheSize int `river:"max_cache_size,attr,optional"`
+}
+
+// DefaultArguments provides the default arguments for the loki.relabel
+// component.
+var DefaultArguments = Arguments{
+	MaxCacheSize: 10_000,
+}
+
+var _ river.Unmarshaler = (*Arguments)(nil)
+
+// UnmarshalRiver implements river.Unmarshaler.
+func (a *Arguments) UnmarshalRiver(f func(interface{}) error) error {
+	*a = DefaultArguments
+
+	type arguments Arguments
+	return f((*arguments)(a))
 }
 
 // Exports holds values which are exported by the discovery.relabel component.
@@ -42,15 +65,26 @@ type Exports struct {
 type Component struct {
 	opts component.Options
 
-	mut sync.RWMutex
-	rcs []*relabel.Config
+	mut          sync.RWMutex
+	rcs          []*relabel.Config
+	cache        *lru.Cache
+	maxCacheSize int
 }
 
 var _ component.Component = (*Component)(nil)
 
 // New creates a new discovery.relabel component.
 func New(o component.Options, args Arguments) (*Component, error) {
-	c := &Component{opts: o}
+	cache, err := lru.New(args.MaxCacheSize)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Component{
+		opts:         o,
+		cache:        cache,
+		maxCacheSize: args.MaxCacheSize,
+	}
 
 	// Call to Update() to set the output once at the start
 	if err := c.Update(args); err != nil {
@@ -75,13 +109,23 @@ func (c *Component) Update(args component.Arguments) error {
 
 	targets := make([]discovery.Target, 0, len(newArgs.Targets))
 	relabelConfigs := flow_relabel.ComponentToPromRelabelConfigs(newArgs.RelabelConfigs)
-	c.rcs = relabelConfigs
+	if relabelingChanged(c.rcs, relabelConfigs) {
+		level.Debug(c.opts.Logger).Log("msg", "received new relabel configs, purging cache")
+		c.cache.Purge()
+		c.rcs = relabelConfigs
+		// c.metrics.cacheSize.Set(0)
+	}
+	if newArgs.MaxCacheSize != c.maxCacheSize {
+		evicted := c.cache.Resize(newArgs.MaxCacheSize)
+		if evicted > 0 {
+			level.Debug(c.opts.Logger).Log("msg", "resizing the cache lead to evicting of items", "len_items_evicted", evicted)
+		}
+	}
 
 	for _, t := range newArgs.Targets {
-		lset := componentMapToPromLabels(t)
-		lset = relabel.Process(lset, relabelConfigs...)
+		lset := c.relabel(t)
 		if lset != nil {
-			targets = append(targets, promLabelsToComponent(lset))
+			targets = append(targets, lset)
 		}
 	}
 
@@ -109,4 +153,65 @@ func promLabelsToComponent(ls labels.Labels) discovery.Target {
 	}
 
 	return res
+}
+
+func relabelingChanged(prev, next []*relabel.Config) bool {
+	if len(prev) != len(next) {
+		return true
+	}
+	for i := range prev {
+		if !reflect.DeepEqual(prev[i], next[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+type cacheItem struct {
+	original  discovery.Target
+	relabeled discovery.Target
+}
+
+// TODO(@tpaschalis) It's unfortunate how we have to cast back and forth
+// between model.LabelSet (map) and labels.Labels (slice).
+func (c *Component) relabel(tgt discovery.Target) discovery.Target {
+	lset := labels.FromMap(tgt)
+	hash := lset.Hash()
+
+	// Let's look in the cache for the hash of the entry's labels.
+	val, found := c.cache.Get(hash)
+
+	// We've seen this hash before; let's see if we've already relabeled this
+	// specific entry before and can return early, or if it's a collision.
+	if found {
+		for _, ci := range val.([]cacheItem) {
+			if labels.Equal(tgt.Labels(), ci.original.Labels()) {
+				// c.metrics.cacheHits.Inc()
+				return ci.relabeled
+			}
+		}
+	}
+
+	// Seems like it's either a new entry or a hash collision.
+	// c.metrics.cacheMisses.Inc()
+	relabeled := c.process(tgt)
+
+	// In case it's a new hash, initialize it as a new cacheItem.
+	// If it was a collision, append the result to the cached slice.
+	if !found {
+		val = []cacheItem{{tgt, relabeled}}
+	} else {
+		val = append(val.([]cacheItem), cacheItem{tgt, relabeled})
+	}
+
+	c.cache.Add(hash, val)
+	// c.metrics.cacheSize.Set(float64(c.cache.Len()))
+
+	return relabeled
+}
+
+func (c *Component) process(tgt discovery.Target) discovery.Target {
+	lset := componentMapToPromLabels(tgt)
+	lset = relabel.Process(lset, c.rcs...)
+	return promLabelsToComponent(lset)
 }
